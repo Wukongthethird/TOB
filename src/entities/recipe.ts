69dc84a54ae13099af377ca53ca23d23c19426d6
Wukongthethird <simonzhang9518@gmail.com>
2022-03-28 17:57:40 -0700

    back

--- src/entities/recipe.ts
+++ src/entities/recipe.ts
@@ -1,28 +1,28 @@
 import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
 
 @Entity()
-export class Recipe  {
+export class Recipe {
   @PrimaryKey()
   id!: number;
 
-  @Property({type:'date', default:'NOW()'})
-  createdAt = new Date()
+  // @Property({type:'date', default:'NOW()'})
+  // createdAt = new Date()
 
-  @Property({onUpdate: ()=>new Date() } )
-  updatedAt = new Date()
-
-  @Property()
-  name!: string; 
+  // @Property({onUpdate: ()=>new Date() } )
+  // updatedAt = new Date()
 
   @Property()
-  description!: string; 
+  name!: string;
 
   @Property()
-  direction!: string; 
+  description!: string;
 
   @Property()
-  summary!: string; 
+  direction!: string;
 
   @Property()
-  ingredients!: string; 
+  summary!: string;
+
+  @Property()
+  ingredients!: string;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,95 @@
-import {MikroORM} from "@mikro-orm/core";
-import { __production__ } from "./constants";
-import { Recipe } from "./entities/recipe";
-import MikroConfig from "./mikro-orm.config"
-import express from 'express' 
-// import {ApolloServer} from 'apollo-server-express'
 
-const MAIN = async() =>{
+import { COOKIE_NAME, __production__ } from "./constants";
+// import { Recipe } from "./entities/recipe";
+import 'reflect-metadata'
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { Hello } from "./resolvers/hello";
+import { UserResolver } from "./resolvers/users";
+import { PostResolver } from "./resolvers/posts";
+import cors from "cors";
+import Redis from "ioredis"
+import { createConnection } from "typeorm";
 
-  const orm = MikroORM.init(MikroConfig);
-  (await orm).getMigrator().up();
-  const recipe =  (await  orm).em.create(
-    Recipe,
-      { name:'Roast Chicken',
-        description:'It is a fkin chicken',
-        direction: '',
-        summary:'',
-        ingredients:'',
-    });
-  (await orm).em.persistAndFlush(recipe)
+
+
+
+// let redis = require("redis");
+
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { Users } from "./entities/Users";
+import { Post } from "./entities/Post";
+
+
+const MAIN = async () => {
+  const conn = await createConnection({
+    type:'postgres',
+    database:'tasteofbukkake2',
+    username: "simonzhang",
+    password: "admin",
+    logging:true,
+    synchronize:true,
+    entities:[Post , Users]
+
+  });
+
   const app = express();
-  app.listen(3000, ()=>{
-    console.log('app is on 3000')
-  })
+  let RedisStore = connectRedis(session);
+  let redis = new Redis()
 
-}
 
+  app.use(
+    cors({
+      // origin: "http://localhost:3000",
+      origin : "*",
+      credentials: true,
+    })
+  );
+
+  app.use(
+    session({
+      name: COOKIE_NAME,
+      store: new RedisStore({
+        client: redis,
+        disableTTL: true,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
+        httpOnly: true,
+        sameSite: "lax",
+        secure: __production__,
+      },
+      saveUninitialized: false,
+      secret: "keyboard cat",
+      resave: false,
+    })
+  );
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [PostResolver, Hello, UserResolver],
+      validate: false,
+    }),
+    context: async ({ req, res }) => ({
+      req,
+      res,
+      redis
+    }),
+  });
+
+  await apolloServer.start();
+  apolloServer.applyMiddleware({
+    app,
+    cors: { origin: false },
+  });
+
+  app.listen(4000, () => {
+    console.log("app is on 4000");
+  });
+};
 
 MAIN().catch((err) => {
-  console.log(err)
-})
-
-console.log('dirname', __dirname);
-
-console.log('hello senpai')
+  console.log(err);
+});
